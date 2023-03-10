0f758f01fd3e7d8e44e22d2aba148b0a94bf725f
Sadat Rahman <rahmans22@coventry.ac.uk>
2023-03-10 13:23:30 +0000

    Created basic authentication strategy using Passport

---
+++ strategies/basic.js
@@ -0,0 +1,36 @@
+const BasicStrategy = require('passport-http').BasicStrategy;
+const users = require('../models/users');
+
+const verifyPassword = function (user, password) {
+    // compare user.password with the password supplied
+    return user.password === password;
+}
+
+const checkUserAndPass = async (username, password, done) => {
+    // look up the user and check the password if the user exists
+    // call done() with either an error or the user, depending on outcome
+    let result;
+  
+    try {
+        result = await users.getByUsername(username);
+    } catch (error) {
+        console.error(`Error during authentication for: ${username}`);
+        return done(error);
+    }
+  
+    if (result.length) {
+        const user = result[0];
+        if (verifyPassword(user, password)) {
+            console.log(`Successfully authenticated: ${username}`);
+            return done(null, user);
+        } else {
+            console.log(`Password incorrect for: ${username}`);
+        }
+    } else {
+        console.log(`No user found with username: ${username}`);
+    }
+    return done(null, false);  // username or password were incorrect
+}
+
+const strategy = new BasicStrategy(checkUserAndPass);
+module.exports = strategy;
