75945e2e44cfefa36b83fb5c5c744557437c98b3
Sadat Rahman <rahmans22@coventry.ac.uk>
2023-03-11 14:55:05 +0000

    Applied permission check in getById

--- routes/users.js
+++ routes/users.js
@@ -31,18 +31,27 @@ async function getAll(ctx) {
     }
 }
 
-  async function getById(ctx) {
+async function getById(ctx) {
     let id = ctx.params.id;
-    let user = await model.getById(id);
-    if (user.length) {
-        ctx.body = user[0];
-        ctx.status = 200;
+    let result = await model.getById(id);
+  
+    if (result.length) {
+        let data = result[0];
+        let permission = can.read(ctx.state.user, data);
+      
+        if (!permission.granted) {
+            ctx.status = 403;
+        } else {
+            let body = permission.filter(data);
+            ctx.body = body;
+            ctx.status = 200;
+        }
     } else {
         ctx.status = 404;
     }
 }
 
-  async function createUser(ctx) {
+async function createUser(ctx) {
     let body = ctx.request.body;
     let result = await model.add(body);
     if (result.affectedRows) {
@@ -53,7 +62,7 @@ async function getAll(ctx) {
     }
 }
 
-  async function updateUser(ctx){
+async function updateUser(ctx){
     let id = ctx.params.id;
     let body = ctx.request.body;
     let result = await model.updateById(body, id);
@@ -65,7 +74,7 @@ async function getAll(ctx) {
     }
 }
 
-  async function deleteUser(ctx){
+async function deleteUser(ctx){
     let id = ctx.params.id;
     let result = await model.deleteById(id);
     if (result.affectedRows) {
