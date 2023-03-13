827e346c5bd3155b02192e00c2837ac97afe9e80
Sadat Rahman <rahmans22@coventry.ac.uk>
2023-03-13 16:30:05 +0000

    Added models to add/remove relations between genres and books

--- models/bookGenres.js
+++ models/bookGenres.js
@@ -8,9 +8,26 @@ exports.getAllGenres = async function getAll (id) {
     return data;
 }
 
+// Get all books that belong to a specific genre
 exports.getAllBooks = async function getAll (id) {
     let query = "SELECT * FROM books as b INNER JOIN bookGenres AS bg ON bg.bookID = b.ID WHERE bg.genreID = ?";
     let values = [id];
+    let data = await db.run_query(query, values);
+    return data;
+}
+
+// Add relation between book and genre.
+exports.addGenre = async function addGenre (bookGenre) {
+    let query = "INSERT INTO bookGenres SET ?";
+    let values = [bookGenre];
+    let data = await db.run_query(query, values);
+    return data;
+}
+
+// Remove relation between book and genre.
+exports.removeGenre = async function removeGenre (bookID, genreID) {
+    let query = "DELETE FROM bookGenres WHERE bookID = ? and genreID = ?";
+    let values = [bookID, genreID];
     let data = await db.run_query(query, values);
     return data;
 }
