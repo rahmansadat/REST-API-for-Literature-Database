5498b1dcead57ff3ba58eabd82b3c69882114078
Sadat Rahman <rahmans22@coventry.ac.uk>
2023-04-07 13:17:43 +0000

    Added test to check if jest uses the right database

--- __tests__/check_jest_works.js
+++ __tests__/check_jest_works.js
@@ -1,9 +1,13 @@
 // This test fails because 1 != 2
 
 it('Testing Jest (should fail)', () => {
-    expect(1).toBe(2)
+    expect(1).toBe(2);
 })
 
 it('Testing Jest (should pass)', () => {
-    expect(1).toBe(1)
+    expect(1).toBe(1);
+})
+
+it('Jest uses the test DB (should pass)', () => {
+    expect(process.env.DB_DATABASE).toBe("test_db");
 })
