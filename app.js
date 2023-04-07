734cf23817f3d689bc8ffb53eb6290ab33bf6f14
Sadat Rahman <rahmans22@coventry.ac.uk>
2023-04-07 14:58:02 +0000

    Refactor index.js to allow for testing

---
+++ app.js
@@ -0,0 +1,17 @@
+const Koa = require('koa');
+
+const books = require('./routes/books.js');
+const genres = require('./routes/genres.js');
+const authors = require('./routes/authors.js');
+const reviews = require('./routes/reviews.js');
+const users = require('./routes/users.js');
+
+const app = new Koa();
+
+app.use(books.routes());
+app.use(genres.routes());
+app.use(authors.routes());
+app.use(reviews.routes());
+app.use(users.routes());
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,19 +1,4 @@
-const Koa = require('koa');
-
-const books = require('./routes/books.js');
-const genres = require('./routes/genres.js');
-const authors = require('./routes/authors.js');
-const reviews = require('./routes/reviews.js');
-const users = require('./routes/users.js');
-
-const app = new Koa();
-
-app.use(books.routes());
-app.use(genres.routes());
-app.use(authors.routes());
-app.use(reviews.routes());
-app.use(users.routes());
-
+const app = require("./app")
 let port = process.env.PORT || 3000;
-
 app.listen(port);
+console.log(`API server running on port ${port}`)
